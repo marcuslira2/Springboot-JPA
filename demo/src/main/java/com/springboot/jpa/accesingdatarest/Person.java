bcd7a103ad1635daf327348d0f8d8193b1cee66a
Marcus Lira <marcuslira57@gmail.com>
2022-02-08 13:08:13 -0300

    Accesing JPA Data with REST

---
+++ demo/src/main/java/com/springboot/jpa/accesingdatarest/Person.java
@@ -0,0 +1,35 @@
+package com.springboot.jpa.accesingdatarest;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String firstName,lastName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+	
+}
